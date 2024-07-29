2f202f0e410e9b24fcd5ffc592bf5af5d739aed7
Nix-code <nishantbanjade07@gmail.com>
2024-07-29 17:38:24 +0545

    update .gitignore file

--- MessageBrokerEngine/MessageBrokerCore/obj/Debug/net8.0/MessageBrokerCore.AssemblyInfo.cs
+++ MessageBrokerEngine/MessageBrokerCore/obj/Debug/net8.0/MessageBrokerCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MessageBrokerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d539639313b3ed153a14a77157e9ceafa5abd181")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48d7fd3a7ec9f2e065819931a4a0521eb32bb102")]
 [assembly: System.Reflection.AssemblyProductAttribute("MessageBrokerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MessageBrokerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
