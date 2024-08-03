8b93247475a8d8e7b9c1d6fe3d5d16630df9f034
Nix-code <nishantbanjade07@gmail.com>
2024-08-03 20:21:10 +0545

    add: class for IP hashing with no implementation

--- MessageBrokerEngine/MessageBrokerCore/Load Balancer/IPAddressHashing.cs
+++ MessageBrokerEngine/MessageBrokerCore/Load Balancer/IPAddressHashing.cs
@@ -5,12 +5,7 @@ namespace MessageBrokerEngine.MessageBrokerCore.LoadBalancer
     public class IPAddressHashing {
         
 
-        // Method to convert ip address to numeric hash value
-        public static int GetHashValue(string ipAddresss){
-            return 0;
-        }
         
-
 
     }
 }
