0e70f27b5f0e4c22f28feea6e9467be6e3baa448
Nix-code <nishantbanjade07@gmail.com>
2024-08-03 20:20:54 +0545

    add: TopicConfiguration Interface for topic specific some optional properties

---
+++ MessageBrokerEngine/MessageBrokerCore/Common/Interfaces/Topics/ITopicConfiguration.cs
@@ -0,0 +1,24 @@
+namespace MessageBrokerEngine.MessageBrokerCore.Common.Interfaces.Topics {
+
+    public class TopicConfiguration{
+
+        public string? TopicDescription { get; set; }
+        public string? TopicRoute { get; set; }
+        public string? TopicOwner { get; set; }
+        public int TopicPartitions { get; set; }
+        public int TopicReplicationFactor { get; set; }
+        public TimeSpan TopicRetentionPeriod { get; set; }
+        public int TopicPriority { get; set; }
+        public TopicType TopicType { get; set; }
+        public IEnumerable<string>? Tags { get; set; }
+    }
+
+    public enum TopicType
+    {
+        Standard,
+        Transactional,
+        Priority,
+        Delayed
+    }
+
+}
