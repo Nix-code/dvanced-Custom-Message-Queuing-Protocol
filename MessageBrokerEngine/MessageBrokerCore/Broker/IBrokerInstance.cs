14c83140a8f1f49183f0356b6218831ab3ab3b7d
Nix-code <nishantbanjade07@gmail.com>
2024-08-03 20:19:43 +0545

    clean up some files with the restructuring and keeping it simple

--- MessageBrokerEngine/MessageBrokerCore/Broker/IBrokerInstance.cs
+++
@@ -1,6 +0,0 @@
-// Producers send messages to Topics.
-// Topics hold messages and route them to appropriate Queues.
-// LoadBalancer balances the load between Producers and Subscribers interacting with the Topics.
-// Queues store the messages until they are processed by Subscribers.
-// Broker Instances manage Queues and ensure efficient message delivery.
-// Communication between all components is handled using TCP, with routing algorithms ensuring efficient message delivery.
