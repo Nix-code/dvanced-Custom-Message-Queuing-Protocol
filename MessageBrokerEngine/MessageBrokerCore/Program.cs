8237f7d4444c949c77d81e2953d5e32d723fe0e8
Nix-code <nishantbanjade07@gmail.com>
2024-08-04 20:29:41 +0545

    update program.cs file

--- MessageBrokerEngine/MessageBrokerCore/Program.cs
+++ MessageBrokerEngine/MessageBrokerCore/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MessageBrokerEngine.MessageBrokerCore.Common.Interfaces.Topics;
+using MessageBrokerEngine.MessageBrokerCore.Infrastructure.Repository;
+using MessageBrokerEngine.MessageBrokerCore.Models;
+
+namespace MessageBrokerEngine.MessageBrokerCore{
+
+    class Program {
+
+        static async Task Main(string[] args)
+        {
+            
+        }
+    }
+}
