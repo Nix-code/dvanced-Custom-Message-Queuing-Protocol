89c27d2c7782b3d6b265c2c9c9dc64d4ae6be1e5
Nix-code <nishantbanjade07@gmail.com>
2024-08-04 20:30:44 +0545

    update  :Message Model class

--- MessageBrokerEngine/MessageBrokerCore/Models/MessageModel.cs
+++ MessageBrokerEngine/MessageBrokerCore/Models/MessageModel.cs
@@ -1,10 +1,10 @@
 namespace MessageBrokerEngine.MessageBrokerCore.Models{
     public class MessageModel
     {
-        Guid Id { get; set; }
-        byte[]? Payload { get; set; }
-        DateTime TimeStamp { get; set; } = DateTime.UtcNow;
-        byte[]? OriginalMessageData { get; set; }
-        int? Priority { get; set; }
+        public Guid Id { get; set; }
+        public byte[]? Payload { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        public byte[]? OriginalMessageData { get; set; }
+        public int? Priority { get; set; }
     }
 }
