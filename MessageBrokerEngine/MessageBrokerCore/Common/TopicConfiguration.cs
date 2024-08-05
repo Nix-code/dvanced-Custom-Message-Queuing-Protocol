4f1f5c04b86174791d78458a37f8d83b4540a03a
Nix-code <nishantbanjade07@gmail.com>
2024-08-05 09:26:22 +0545

    update topicConfigurations

---
+++ MessageBrokerEngine/MessageBrokerCore/Common/TopicConfiguration.cs
@@ -0,0 +1,25 @@
+namespace MessageBrokerEngine.MessageBrokerCore.Common{
+
+    public class TopicConfiguration{
+
+       // public IEnumerable<MessageModel> Messages { get; set; }
+        public string? TopicDescription { get; set; }
+        public string? TopicRoute { get; set; }
+        public string? TopicOwner { get; set; }
+        public int TopicPartitions { get; set; }
+        public int TopicReplicationFactor { get; set; }
+        public TimeSpan TopicRetentionPeriod { get; set; }
+        public int TopicPriority { get; set; }
+        public TopicType TopicType { get; set; }
+        public IEnumerable<string>? Tags { get; set; }
+    }
+
+    public enum TopicType
+    {
+        Standard,
+        Transactional,
+        Priority,
+        Delayed
+    }
+
+}
